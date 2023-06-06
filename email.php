15f579a2c3839704c788533491cbe59f2e5fbd94
XH-Liao <peter020104@gmail.com>
2023-06-06 21:38:08 +0800

    complete 寄送催繳費用Email

--- email.php
+++ email.php
@@ -1,9 +1,4 @@
 <?php
-//POST的資料
-$email = "a1095527@mail.nuk.edu.tw";
-$uName = "廖習驊";
-//$message = $_POST["message"];
-//$message = nl2br($message);
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
@@ -11,6 +6,7 @@ use PHPMailer\PHPMailer\Exception;
 require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
+require_once("dbconnect.php");
 
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
@@ -29,19 +25,40 @@ try {
     $mail->CharSet = 'utf-8';
 
     //Recipients
-    $mail->setFrom('PHPmailNUK@gmail.com', 'DMS_Manager 宿舍管理員');
-    $mail->addAddress($email, $uName);
+    $mail->setFrom('PHPmailNUK@gmail.com', 'DMS_Manager 宿舍管理員');       // 設定寄件者資料
 
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = '宿舍費用催繳通知';
-    $message = $uName . "同學：<br> 同學您好，目前尚未收到您的宿舍費用，請在<font color='red'>7日內</font>繳納費用！<br>" .
-        " 若未在規定期限內繳費，將依宿舍規定第5條規範，予以<font color='red'>退宿</font><br><br><br><br><br>" .
-        "<font color='blue'>此信件為測試訊息，請勿當真！</font>";
-    $mail->Body    = $message;
+    // 設定信件模板內容
+    $content = "同學：<br>" .
+        " 同學您好，目前尚未收到您的宿舍費用，請在<font color='red'>7日內</font>繳納費用！<br>" .
+        " 若未在規定期限內繳費，將依宿舍規定第5條規範，予以<font color='red'>退宿</font><br><br><br>" .
+        "-----------------------------------------------------------------------<br>" .
+        "宿舍管理員 DMS manager<br>" .
+        "國立高雄大學 | National University of Kaohsiung<br>" .
+        "高雄市楠梓區高雄大學路700號<br>" .
+        "Tel：(07)5919000<br>" .
+        "Fax：(07)5919083<br>" .
+        "<br><br><br><br><br><font color='blue'>此信件僅為測試訊息！</font>";
 
-    $mail->send();
-    echo '<h1>訊息已傳送！</h1>';
+    // SQL 查詢所有未繳費的名單 (姓名、Email)
+    $SQL = "SELECT 姓名, Email
+            FROM 入住申請, 學生
+            WHERE 入住申請.學號=學生.學號 AND 繳費狀態=FALSE";
+    $result = mysqli_query($link, $SQL);
+    while ($row = mysqli_fetch_assoc($result)) {
+        $uName = $row["姓名"];
+        $email = $row["Email"];
+
+        $mail->addAddress($email, $uName);                      // 設定收件人
+
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = '宿舍費用催繳通知';
+        $message = $uName . $content;
+        $mail->Body    = $message;
+
+        $mail->send();
+    }
+
     echo "<script type='text/javascript'>alert('提示：催繳Email寄送成功!')</script>";
     echo "<meta http-equiv='Refresh'; content='0; url=assign_room_apply_list'/>";
 } catch (Exception $e) {
