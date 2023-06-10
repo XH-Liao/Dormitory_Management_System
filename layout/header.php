40dcde93eabcf3a0adb2fa3a6760fc334afd3d60
a1095553 <129036677+a1095553@users.noreply.github.com>
2023-06-11 02:18:52 +0800

    Update header.php

--- layout/header.php
+++ layout/header.php
@@ -30,13 +30,13 @@ if ($title != "密碼修改" && isset($_SESSION["login_identity"]) && $_SESSION[
         $(document).ready(function() {
             var location = window.location.href;
             var id = location.substring(location.lastIndexOf('/') + 1);
-            if(id=="stu_list" || id=="admin_list" ||id=="enroll"){
+            if (id == "stu_list" || id == "admin_list" || id == "enroll") {
                 $("#people").addClass("active");
-            }else if(id=="moni_list" || id=="violate_list"){
+            } else if (id == "moni_list" || id == "violate_list") {
                 $("#monitor_violate").addClass("active");
-            }else if(id=="apply" || id=="applied"){
+            } else if (id == "apply" || id == "applied") {
                 $("#apply").addClass("active");
-            }else{
+            } else {
                 $("#" + id).addClass("active");
             }
         });
@@ -142,8 +142,8 @@ EOT;
                                         </a>
                                     </li>
 EOT;
-                                    if ($_SESSION["login_account"] == "admin") {
-                                        print <<< EOT
+                            if ($_SESSION["login_account"] == "admin") {
+                                print <<< EOT
                                         <li class="nav-item">
                                             <a class="dropdown-item" href="admin_list">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="20" fill="currentColor" class="bi bi-person-fill-gear" viewBox="0 0 16 16">
@@ -153,8 +153,8 @@ EOT;
                                             </a>
                                         </li>
 EOT;
-                                    }
-                                    print <<< EOT
+                            }
+                            print <<< EOT
                                     <li class="nav-item">
                                         <a class="dropdown-item" href="enroll" id='enroll'>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="20"  fill="currentColor" class="bi bi-person-plus-fill" viewBox="0 0 16 16">
@@ -207,6 +207,32 @@ EOT;
                                     </svg>
                                     大樓管理
                                 </a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a href="#" class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" id="monitor_violate">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" fill="currentColor" class="bi bi-flag-fill" viewBox="0 0 16 16">
+                                        <path d="M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12.435 12.435 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A19.626 19.626 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a19.587 19.587 0 0 0 1.349-.476l.019-.007.004-.002h.001"/>
+                                    </svg>
+                                    報修
+                                </a>
+                                <ul class="dropdown-menu"> <!-- dropdown-menu-right -->
+                                    <li>
+                                        <a class="dropdown-item" href="fix">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="18"  fill="currentColor" class="bi bi-person-lines-fill" viewBox="0 0 16 16">
+                                                <path fill-rule="evenodd" d="M10 1.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-1Zm-5 0A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5v1A1.5 1.5 0 0 1 9.5 4h-3A1.5 1.5 0 0 1 5 2.5v-1Zm-2 0h1v1A2.5 2.5 0 0 0 6.5 5h3A2.5 2.5 0 0 0 12 2.5v-1h1a2 2 0 0 1 2 2V14a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V3.5a2 2 0 0 1 2-2Z"/>
+                                            </svg>
+                                            報修申請
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="dropdown-item" href="fix_list">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
+                                                <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z" />
+                                            </svg>
+                                            報修列表
+                                        </a>
+                                    </li>
+                                </ul>
                             </li>
                             
 EOT;
