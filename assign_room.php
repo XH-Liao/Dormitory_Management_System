13fcb3fdcbe6d1adb8d14c335de4f71f71cb2215
XH-Liao <peter020104@gmail.com>
2023-06-11 16:29:44 +0800

    update 入住申請的介面優化-下拉式選單

--- assign_room.php
+++ assign_room.php
@@ -12,6 +12,33 @@ require('layout/header.php');
 require("dbconnect.php");
 ?>
 
+<script>
+    $(document).ready(function() {
+        // 綁定第一個下拉式選單的change事件
+        $('#firstSelect').change(function() {
+            var firstValue = $(this).val(); // 取得所選擇的值
+
+            // 根據選擇的值動態調整第二個下拉式選單的選項
+            if (firstValue === '') {
+                $('#secondSelect').html('<option value="">退宿</option>'); // 退宿
+            } else {
+                // 使用AJAX向PHP發送請求，獲取第二個下拉式選單的選項內容
+                $.ajax({
+                    url: "get_second_options.php",
+                    method: "POST",
+                    data: {
+                        value: firstValue
+                    },
+                    success: function(response) {
+                        var secondSelect = document.getElementById("secondSelect");
+                        secondSelect.innerHTML = response;
+                    }
+                });
+            }
+        });
+    });
+</script>
+
 <!-- 警告圖示 -->
 <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
     <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
@@ -135,13 +162,25 @@ EOT;
                 <div class="mb-3 row">
                     <label class="col-sm-2 col-form-label">宿舍編號</label>
                     <div class="col-sm-10">
-                        <input type="text" name="宿舍編號" class="form-control">
+                        <select id="firstSelect" class="form-select" name="宿舍編號" aria-label="Default select example">
+                            <option value="" selected disabled>請選擇</option>
+                            <option value="">退宿</option>
+                            <?php
+                            $SQL = "SELECT * FROM 宿舍大樓";
+                            $result = mysqli_query($link, $SQL);
+                            while ($row = mysqli_fetch_assoc($result)) {
+                                echo "<option value='{$row['宿舍編號']}'>{$row['宿舍編號']}</option>";
+                            }
+                            ?>
+                        </select>
                     </div>
                 </div>
                 <div class="mb-3 row">
                     <label class="col-sm-2 col-form-label">房間號碼</label>
                     <div class="col-sm-10">
-                        <input type="text" name="房間號碼" class="form-control">
+                        <select id="secondSelect" class="form-select" name="房間號碼">
+                            <option value="">請選擇</option>
+                        </select>
                     </div>
                 </div>
                 <input type="submit" value="確認設定寢室" class="form-control btn btn-primary">
