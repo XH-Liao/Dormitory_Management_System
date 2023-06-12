6d5bfff972bfe17ff4192c162266d9b404c39c16
XH-Liao <peter020104@gmail.com>
2023-06-13 01:49:03 +0800

    update 新增班級列表

--- layout/header.php
+++ layout/header.php
@@ -175,6 +175,14 @@ EOT;
                                             老師列表
                                         </a>
                                     </li>
+                                    <li>
+                                        <a class="dropdown-item" href="class_list" id='stu_list'>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="18" fill="currentColor" class="bi bi-collection" viewBox="0 0 16 16">
+                                                <path d="M2.5 3.5a.5.5 0 0 1 0-1h11a.5.5 0 0 1 0 1h-11zm2-2a.5.5 0 0 1 0-1h7a.5.5 0 0 1 0 1h-7zM0 13a1.5 1.5 0 0 0 1.5 1.5h13A1.5 1.5 0 0 0 16 13V6a1.5 1.5 0 0 0-1.5-1.5h-13A1.5 1.5 0 0 0 0 6v7zm1.5.5A.5.5 0 0 1 1 13V6a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-.5.5h-13z"/>
+                                            </svg>
+                                            班級列表
+                                        </a>
+                                    </li>
 EOT;
                             if ($_SESSION["login_account"] == "admin") {
                                 print <<< EOT
